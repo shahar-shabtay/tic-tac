cc4d86d4c2e452e8e41f07e5387dd2c1b9a7464c
shahar-shabtay <shahar.shabtay28@gmail.com>
2024-12-21 13:07:15 +0200

    working layouts

--- app/src/main/java/com/example/tic_tac/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac/MainActivity.kt
@@ -1,20 +1,83 @@
 package com.example.tic_tac
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tic_tac.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    enum class Turn {
+        NOUGHT,
+        CROSS
+    }
+    private var firstTurn = Turn.CROSS
+    private var currentTurn = Turn.CROSS
+
+    private var boardList= mutableListOf<Button>()
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        initBoard()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun initBoard() {
+        boardList.add(binding.a1)
+        boardList.add(binding.a2)
+        boardList.add(binding.a3)
+        boardList.add(binding.b1)
+        boardList.add(binding.b2)
+        boardList.add(binding.b3)
+        boardList.add(binding.c1)
+        boardList.add(binding.c2)
+        boardList.add(binding.c3)
+
+    }
+
+    fun boardTapped(view: View) {
+        if (view !is Button)
+            return
+        addToBoard(view)
+    }
+
+    private fun addToBoard(button: Button) {
+        if (button.text != "")
+            return
+        if (currentTurn == Turn.NOUGHT){
+            button.text = NOUGHT
+            currentTurn = Turn.CROSS
+        }
+        else if (currentTurn == Turn.CROSS){
+            button.text = CROSS
+            currentTurn = Turn.NOUGHT
+        }
+        setTurnLabel()
+    }
+
+    private fun setTurnLabel() {
+        var turnText = ""
+        if (currentTurn == Turn.CROSS)
+            turnText = "Turn $CROSS"
+        else if (currentTurn == Turn.NOUGHT)
+            turnText = "Turn $NOUGHT"
+        binding.turnTV.text= turnText
+    }
+
+
+    companion object {
+        const val NOUGHT = "O"
+        const val CROSS = "X"
     }
 }
